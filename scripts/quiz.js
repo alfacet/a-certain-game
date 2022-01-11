b04af0a1f688c5e003bac3251e232329fc5c4789
Arthur Bernardo <86016100+ArthurBernardoxz@users.noreply.github.com>
2022-01-11 18:39:43 -0300

    Alterações 11/01/2022
    Alterações feitas: criamos vetor para guarda o numero de perguntas o que funcionou para separar as 10 perguntas da sessão e já esta sendo possivel mudar as perguntas
    * Agrupamos as perguntas em respostas certas

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -6,7 +6,7 @@ let BancodePerguntas = [
         b: 'Vermelho',
         c: 'Paralelepipedo',
         d: 'Avião do Silvio Santos',
-        resposta: 1
+        resposta: 'a'
     },
     {
         nPergunta: 2,
@@ -14,7 +14,8 @@ let BancodePerguntas = [
         a: 'Massacre chinês',
         b: 'Nada',
         c: 'Não sei o que é essa praça',
-        d: 'Impossível saber'
+        d: 'Impossível saber',
+        resposta: 'a'
     },
     {
         nPergunta: 3,
@@ -22,15 +23,17 @@ let BancodePerguntas = [
         a: 'Nenhum',
         b: '-1',
         c: '6',
-        d: 'Palmeiras não é time'
+        d: 'Palmeiras não é time',
+        resposta: 'a'
     },
     {
         nPergunta: 4,
-        pergunta: 'Quantas Copa o Brasil tem?',
+        pergunta: 'Quantas Copas do mundo o Brasil tem?',
         a: '6',
         b: '5',
         c: 'Nenhum',
-        d: '4'
+        d: '4',
+        resposta: 'a'
     },
     {
         nPergunta: 5,
@@ -39,17 +42,371 @@ let BancodePerguntas = [
         b: '2002',
         c: '1998',
         d: '1962',
+        resposta: 'a'
     },
+    {
+        nPergunta: 6,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'a'
+    },
+    {
+        nPergunta: 7,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'a'
+    },
+    {
+        nPergunta: 8,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'a'
+    },
+    {
+        nPergunta: 9,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'a'
+    },
+    {
+        nPergunta: 10,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'a'
+    },
+    {
+        nPergunta: 11,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 12,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 13,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 14,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 15,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 16,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 17,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 18,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 19,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 20,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'b'
+    },
+    {
+        nPergunta: 21,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 22,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 23,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 24,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 25,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 26,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 27,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 28,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 29,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 30,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'c'
+    },
+    {
+        nPergunta: 31,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 32,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 33,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 34,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 35,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 36,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 37,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 38,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 39,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4',
+        resposta: 'd'
+    },
+    {
+        nPergunta: 40,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+        resposta: 'd'
+    },
+
 ];
 
 //API que gera número de 0 49
 
-const MAX = 50;
+let numeroJaSorteados = [];
+let numeroAtual = -1;
+let nPerguntas = [];
 
-function getRandomIntInclusive() { //gera número áleatorio para selicionar pergunta
+function getRandomIntInclusive() {
     let min = Math.ceil(0);
-    let max = Math.floor(4);
+    let max = Math.floor(40);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let x = getRandomIntInclusive(); //recebe o N da pergunta
+function sortearpergunta() {
+    if (numeroAtual = -1){
+        numeroAtual = getRandomIntInclusive();
+        numeroJaSorteados.push(numeroAtual);
+        nPerguntas.push(numeroAtual);
+        return;
+    }
+    numeroAtual = getRandomIntInclusive();
+    verificarRepetição(numeroAtual);
+}
+
+function verificarRepetição(nmr_A)
+{
+    for (let i = 0; i < numeroJaSorteados.length; i++) {
+        let cont = 0;
+        if (nmr_A == numeroJaSorteados[i]) {
+            nmr_A == getRandomIntInclusive();
+            verificarRepetição();
+        }
+        cont++;
+    }
+    if(cont == numeroJaSorteados.length){
+        nPerguntas.push(nmr_A);
+        numeroJaSorteados.push(nmr_A);
+    }
+}
+
+for(let i = 0; i < 10; i++){
+    sortearpergunta();
+}
+
+
+Pergunta_atual = document.querySelector('#pergunta');
+console.log(Pergunta_atual);
+
+Pergunta_atual.innerHTML = BancodePerguntas[nPerguntas[0]].pergunta;
