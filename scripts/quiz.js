f33a906e8bce65f28cd64715d7ff1e9a25a0c6ee
Filipe Davi <filipedavi80@gmail.com>
2022-01-11 16:39:43 -0300

    commit de ontem

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,50 +1,55 @@
-let pergunta = document.getElementById('pergunta');
-let escolha = document.querySelectorAll('.containerPergunta');
-let progressoT = document.getElementById('progressoTexto'); /*texto de progresso*/
-let scoreT = document.getElementById('score'); /*pontuação*/
-let progressoB = document.getElementById('progressoBarraComp'); /*barra de progresso*/
+let BancodePerguntas = [
+    {
+        nPergunta: 1,
+        pergunta: 'Qual a cor do céu?',
+        a: 'Azul',
+        b: 'Vermelho',
+        c: 'Paralelepipedo',
+        d: 'Avião do Silvio Santos',
+        resposta: 1
+    },
+    {
+        nPergunta: 2,
+        pergunta: 'O que aconteceu na praça Tiananmen em 1989?',
+        a: 'Massacre chinês',
+        b: 'Nada',
+        c: 'Não sei o que é essa praça',
+        d: 'Impossível saber'
+    },
+    {
+        nPergunta: 3,
+        pergunta: 'Quantos mundial o Palmeiras tem?',
+        a: 'Nenhum',
+        b: '-1',
+        c: '6',
+        d: 'Palmeiras não é time'
+    },
+    {
+        nPergunta: 4,
+        pergunta: 'Quantas Copa o Brasil tem?',
+        a: '6',
+        b: '5',
+        c: 'Nenhum',
+        d: '4'
+    },
+    {
+        nPergunta: 5,
+        pergunta: 'Em que ano o Brasil foi Tetracampeão',
+        a: '1994',
+        b: '2002',
+        c: '1998',
+        d: '1962',
+    },
+];
 
-let perguntaAtual = {};
-let aceitaResp = true;
-let score = 0;
-let cont = 0;
-let perguntasDisp = [];
+//API que gera número de 0 49
 
-let perguntas = [ 
-    {
-        pergunta = 'uudkfjhaksfbhasdkfgasdifg?',
-        escolha1 = 'b',
-        escolha2 = 'g',
-        escolha3 = 'h',
-        escolha1 = 'j',
-        resposta = 1,
-        assunto = 'historia',
-    },
-    {
-        pergunta = 'uudkfjhaksfbhasdkfgasdifg?',
-        escolha1 = 'b',
-        escolha2 = 'g',
-        escolha3 = 'h',
-        escolha1 = 'j',
-        resposta = 2,
-        assunto = 'geografia',
-    },
-    {
-        pergunta = 'uudkfjhaksfbhasdkfgasdifg?',
-        escolha1 = 'b',
-        escolha2 = 'g',
-        escolha3 = 'h',
-        escolha1 = 'j',
-        resposta = 3,
-        assunto = 'inglês',
-    },
-    {
-        pergunta = 'uudkfjhaksfbhasdkfgasdifg?',
-        escolha1 = 'b',
-        escolha2 = 'g',
-        escolha3 = 'i',
-        escolha1 = 'j',
-        resposta = 4,
-        assunto = 'física',
-    }
-]
+const MAX = 50;
+
+function getRandomIntInclusive() { //gera número áleatorio para selicionar pergunta
+    let min = Math.ceil(0);
+    let max = Math.floor(4);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let x = getRandomIntInclusive(); //recebe o N da pergunta
