53d11180f6d4380e80c0a645da7ff7d41875292a
Filipe Davi <filipedavi80@gmail.com>
2022-01-18 08:40:19 -0300

    Inserção do banco de perguntas e novo JavaScript

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,425 +1,96 @@
-let bancoDePerguntas = [
-    {
-        nPergunta: 1,
-        pergunta: 'Quais o menor e o maior país do mundo?',
-        a: 'Vaticano e Rússia',
-        b: 'Nauru e China',
-        c: 'Mônaco e Canadá',
-        d: 'Rússia e União dos Complexos do Rio de Janeiro',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 2,
-        pergunta: 'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
-        a: 'João Goulart',
-        b: 'Jacinto Pinto',
-        c: 'Kleber Machado',
-        d: 'Jânio Quadros',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 3,
-        pergunta: 'Quantos mundial o Palmeiras tem?',
-        a: 'Nenhum',
-        b: '-1',
-        c: '6',
-        d: 'Palmeiras não é time',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 4,
-        pergunta: 'Quantas Copas do mundo o Brasil tem?',
-        a: '5',
-        b: '6',
-        c: 'Nenhum',
-        d: '4',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 5,
-        pergunta: 'Em que ano o Brasil foi Tetracampeão',
-        a: '1994',
-        b: '2002',
-        c: '1998',
-        d: '1962',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 6,
-        pergunta: 'Qual o livro mais vendido no mundo a seguir à Bíblia?',
-        a: 'Dom Quixote',
-        b: 'O Senhor dos Anais',
-        c: 'Extraordinário',
-        d: 'Um Conto de Duas Cidades',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 7,
-        pergunta: 'Quantas casas decimais tem o número pi?',
-        a: 'Infinitas',
-        b: '20',
-        c: '69',
-        d: '101',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 8,
-        pergunta: 'Atualmente, quantos elementos químicos a tabela periódica possui?',
-        a: '118',
-        b: '100',
-        c: '156',
-        d: '212',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 9,
-        pergunta: 'O que a palavra legend significa em português?',
-        a: 'Lenda',
-        b: 'Legenda',
-        c: 'Londrina',
-        d: 'Lombriga',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 10,
-        pergunta: 'Qual o número mínimo de jogadores numa partida de futebol?',
-        a: '7',
-        b: '8 jogadores e meio',
-        c: '2 atacantes 1 goleiro e 2 gandulas',
-        d: '11',
-        resposta: 'a'
-    },
-    {
-        nPergunta: 11,
-        pergunta: 'Quem foi o primeiro presidente dos Estados Unidos',
-        a: 'Donald Trump',
-        b: 'George Washington',
-        c: 'Abraham Lincoln',
-        d: 'Lula',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 12,
-        pergunta: 'Quantas bolas de ouro o Cristiano Ronaldo tem?',
-        a: '4',
-        b: '5',
-        c: '7',
-        d: '3',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 13,
-        pergunta: 'Quantas casas decimais tem o número pi?',
-        a: 'Duas',
-        b: 'Infinitas',
-        c: 'Vinte',
-        d: 'Centenas',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 14,
-        pergunta: 'Qual a nacionalidade de Che Guevara?',
-        a: 'Peruana',
-        b: 'Argentina',
-        c: 'Boliviana',
-        d: 'Peruana',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 15,
-        pergunta: 'Qual o maior animal terrestre?',
-        a: 'Baleia Azul',
-        b: 'Elefante Africano',
-        c: 'Tubarão Branco',
-        d: 'Girafa',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 16,
-        pergunta: 'Que animal gruguleja?',
-        a: 'Passarinho',
-        b: 'Peru',
-        c: 'Leão',
-        d: 'Cacatua',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 17,
-        pergunta: 'Quem pintou o quadro Mona Lisa?',
-        a: 'Alejadinho',
-        b: 'Leonardo Da Vinci',
-        c: 'Vincent Van Gogh',
-        d: 'Rafael Sanzio',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 18,
-        pergunta: 'Quem foi o vencedor do Big Brother Brasil 20?',
-        a: 'Prior',
-        b: 'Thelma Assis',
-        c: 'Petrix',
-        d: 'Hadballa',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 19,
-        pergunta: 'Qual fruta envenenada comeu a Branca de Neve?',
-        a: 'Uva',
-        b: 'Maça',
-        c: 'Banana',
-        d: 'Tomate',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 20,
-        pergunta: 'Qual é o maior país do mundo?',
-        a: 'Brasil',
-        b: 'Rússia',
-        c: 'Japão',
-        d: 'China',
-        resposta: 'b'
-    },
-    {
-        nPergunta: 21,
-        pergunta: 'Qual das alternativas abaixo não é uma obra Barroca',
-        a: 'As Meninas',
-        b: 'A Leiteira',
-        c: 'Two girls one Cup',
-        d: 'A Ronda Noturna',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 22,
-        pergunta: 'Qual a montanha mais alta do Brasil?',
-        a: 'Pico Paraná',
-        b: 'Monte Roraima',
-        c: 'Pico da Neblina',
-        d: 'Pico da Bandeira',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 23,
-        pergunta: 'Em qual local da Ásia o português é língua oficial?',
-        a: 'Portugal',
-        b: 'Índia',
-        c: 'Macau',
-        d: 'Moçambique',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 24,
-        pergunta: 'Qual o maior animal terrestre?',
-        a: 'Dinossauro',
-        b: 'Girafa',
-        c: 'Elefante Africano',
-        d: 'Baleia Azul',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 25,
-        pergunta: 'Qual o tema do famoso discurso Eu Tenho um Sonho, de Martin Luther King?',
-        a: 'Prêmio Nobel da Paz',
-        b: 'Intolerância religiosa',
-        c: 'Igualdade das raças',
-        d: 'Luta contra o Apartheid',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 26,
-        pergunta: 'Quais os nomes dos três Reis Magos?',
-        a: 'Renan Rosandi, Juracinto e Clovis',
-        b: 'Baltazar, Chico Barney e Clitoris',
-        c: 'Belchior, Gaspar e Baltazar',
-        d: 'João Kleber, Gilberto Barros e Siqueira Junior',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 27,
-        pergunta: 'Que animal gruguleja?',
-        a: 'A Cacatua',
-        b: 'A Garça',
-        c: 'O Peru',
-        d: 'Impossível saber',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 28,
-        pergunta: 'Qual é o maior arquipélago da Terra?',
-        a: 'As Maldivas',
-        b: 'A Filipinas',
-        c: 'A Indonêsia',
-        d: 'As Bahamas',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 29,
-        pergunta: 'Qual o metal cujo símbolo químico é o Au?',
-        a: 'Cobre',
-        b: 'Prata',
-        c: 'Ouro',
-        d: 'Manganês',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 30,
-        pergunta: 'Em que oceano fica Madagascar?',
-        a: 'Oceano Ártico',
-        b: 'Oceano Atlântico',
-        c: 'Oceano Índico',
-        d: 'Oceano Antártico',
-        resposta: 'c'
-    },
-    {
-        nPergunta: 31,
-        pergunta: 'Qual personagem do folclore brasileiro que tem os pés virados para trás?',
-        a: 'Carol Conká',
-        b: 'Chico Barney',
-        c: 'Saci',
-        d: 'O Curupira',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 32,
-        pergunta: 'Qual é o planeta mais próximo do sol?',
-        a: 'Venûs',
-        b: 'Marte',
-        c: 'Plutão',
-        d: 'Mercúrio',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 33,
-        pergunta: 'Quantos centímetros tem um metro?',
-        a: '60',
-        b: '80',
-        c: '70',
-        d: '100',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 34,
-        pergunta: 'Em que ano aconteceu o acidente na Usina Nuclear de Chernobyl?',
-        a: 'Em 1968',
-        b: 'Em 1967',
-        c: 'Em 1958',
-        d: 'Em 1986',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 35,
-        pergunta: 'Quantas estrelas tem a bandeira do Brasil?',
-        a: '24 estrelas ',
-        b: '28 estrelas',
-        c: '29 estrelas',
-        d: '27 estrelas. Elas representam os 26 estados brasileiros e o Distrito Federal.',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 36,
-        pergunta: 'Quantos corações tem um polvo?',
-        a: 'Quatro corações',
-        b: 'Um coração',
-        c: 'Dois corações',
-        d: 'Três corações.',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 37,
-        pergunta: 'Quem foi o primeiro brasileiro a conquistar um cinturão no UFC?',
-        a: 'Massacre chinês',
-        b: 'Nada',
-        c: 'Não sei o que é essa praça',
-        d: 'O lutador Murilo Bustamante conquistou o cinturão em 2002, na categoria dos meio-pesados (até 93 kg).',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 38,
-        pergunta: 'Que país tem o formato de uma bota?',
-        a: 'Nenhum',
-        b: '-1',
-        c: '6',
-        d: 'Palmeiras não é time',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 39,
-        pergunta: 'Quantas Copa o Brasil tem?',
-        a: '6',
-        b: '5',
-        c: 'Nenhum',
-        d: '4',
-        resposta: 'd'
-    },
-    {
-        nPergunta: 40,
-        pergunta: 'Em que ano o Brasil foi Tetracampeão',
-        a: '1994',
-        b: '2002',
-        c: '1998',
-        d: '1962',
-        resposta: 'd'
-    },
-];
+function arquivoJson() {
+    let file = new XMLHttpRequest();
+  
+    file.open("GET", "banco.json", false); 
+    file.send();
+    return file.response;
+} //carrega banco de perguntas do json
+  
+let perguntas = JSON.parse(arquivoJson());
+  
 
-//API que gera número de 0 49
-let numeroJaSorteados = [];
-let numeroAtual = -1;
-let nPerguntas = [];
+const perguntaEl = document.getElementById("pergunta-questao");
+const respostasEl = Array.from(document.getElementsByClassName("resposta"));
+const contadorDePerguntasTextoEl = document.getElementById("contador");
+const pontosEl = document.getElementById("pontos");
+//elementos retirados do html
 
-function getRandomIntInclusive() {
-    let min = Math.ceil(0);
-    let max = Math.floor(40);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+let contadorDePergunta;
+let pontos;
+const MAX = 2; //máximo de perguntas
+let aceitaRespostas;
 
-function sortearPergunta() {
-    if (numeroAtual = -1){
-        numeroAtual = getRandomIntInclusive();
-        numeroJaSorteados.push(numeroAtual);
-        nPerguntas.push(numeroAtual);
-        return;
-    }
-    numeroAtual = getRandomIntInclusive();
-    verificarRepetição(numeroAtual);
-}
-
-function verificarRepetição(nmr_A) {
-    for (let i = 0; i < numeroJaSorteados.length; i++) {
-        let cont = 0;
-        if (nmr_A == numeroJaSorteados[i]) {
-            nmr_A == getRandomIntInclusive();
-            verificarRepetição();
-        }
-        cont++;
-    }
-    if (cont == numeroJaSorteados.length){
-        nPerguntas.push(nmr_A);
-        numeroJaSorteados.push(nmr_A);
-    }
-}
-
-for (let i = 0; i < 10; i++){
-    sortearPergunta();
-}
-
-
-pergunta_atual = document.querySelector('#pergunta');
-console.log(pergunta_atual);
-
-pergunta_atual.innerHTML = bancoDePerguntas[nPerguntas[0]].pergunta;
-
-
-//função que aplica nova questão
-const max = 40;
-
-start = () => {
-    cont = 0;
+startQuiz = () => {
+    contadorDePergunta = 0;
     pontos = 0;
-    questoes = bancoDePerguntas;
-}
+    aceitaRespostas = true;
+    perguntasDisp = perguntaAleatoria(perguntas, MAX);  //perguntas disponiveis
+    pontosEl.innerHTML = pontos;
 
-gerarPergunta = () => {
-    if (bancoDePerguntas.length = 0 || cont > max) {
-        localStorage.setItem('pontos', pontos);
+    novaPergunta();
+};
+//função setinha para começar o quiz
+
+const perguntaAleatoria = (objeto, n) => {
+    if (n > objeto.length) {
+      throw new RangeError(
+        "perguntaAleatoria: mais elementos do que disponiveis"
+      );
+    }  //acontecerá o erro de "memoria aplicada", por assim dizer
+  
+    const gerado = [...objeto].sort(() => 0.5 - Math.random());
+  
+    return (selecionado = gerado.slice(0, n));
+};
+
+const novaPergunta = () => {
+    if (perguntasDisp.length == 0) {
+        alert("Fim do Quiz");
+        return;
+
+        //criar função para retornar para o perfil do usuario
     }
-}
+
+    contadorDePergunta++;
+    contadorDePerguntasTextoEl.innerHTML = `${contadorDePergunta}/${MAX}`;
+    perguntaAtual = perguntasDisp[0];
+    perguntaEl.innerHTML = perguntaAtual.pergunta; //recebe o valor do array de objetos
+
+    respostasEl.forEach((resposta) => {
+        resposta.innerHTML = perguntaAtual[resposta.dataset["resposta"]];
+    });
+
+
+    respostasEl.forEach((resposta) => {
+        resposta.addEventListener("click", (e) => {
+            if (!aceitaRespostas) {
+                return;
+            }
+
+            aceitaRespostas = false;
+
+            const respostaClicada = e.target;
+            const respostaDefinida = respostaClicada.dataset["resposta"];
+
+            let estado = "Errado";
+
+            if (respostaDefinida == perguntaAtual.resposta) {
+                pontos++;
+                pontosEl.innerHTML = pontos;
+                estado = "Certo";
+            }
+
+            respostaClicada.parentElement.classList.add(estado);
+            
+            setTimeout(() => { //api para executar uma função em determinado tempo
+                respostaClicada.parentElement.classList.remove(estado);
+                novaPergunta();
+                aceitaRespostas = true;
+            }, 100);
+        });
+    });
+
+    perguntasDisp.shift();
+};
+
+startQuiz();
