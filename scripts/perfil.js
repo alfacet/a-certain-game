11785ead18e104055eb79ad60147c904f623ddd8
TulioDel <tuliodeladio@gmail.com>
2022-02-01 19:52:22 -0300

    Começo do ranking
    mudanças

--- scripts/perfil.js
+++ scripts/perfil.js
@@ -1,50 +1,61 @@
 let nome_de_usuario = document.querySelector("#Nusuario");
 let pontos = document.querySelector("#Npontos");
 let usuarios = [];
-
-carregar();
 let usuario_atual;
-function carregar() {
+let fotoPerfilEl = document.querySelector("#fotoPerfil");
+let selecionar_foto = document.querySelector("#pers_perfil");
 
+
+function carregar() {
     let y = localStorage.getItem('usuarios');
-    let usuario_atual = sessionStorage.getItem('usuario_atual');
+    usuario_atual = sessionStorage.getItem('usuario_atual');
     y = JSON.parse(y);
     if (y != null)
         usuarios = y;
     nome_de_usuario.innerHTML = usuarios[usuario_atual].nome;
     pontos.innerHTML = usuarios[usuario_atual].pontos;
-    mudar_foto_perfil;
+    mudar_foto_perfil(usuarios[usuario_atual].personagem);
 }
-let fotoPerfilEl = document.querySelector("#fotoPerfil");
-let selecionar_foto = document.querySelector("#pers_perfil");
 
-selecionar_foto.addEventListener("change", mudar_foto_perfil);
 
-function mudar_foto_perfil() {
-    let personagem = selecionar_foto.value;
 
+
+
+
+function mudar_foto_perfil(personagem) {
+    
     if(personagem == 0){
         fotoPerfilEl.src = "imgs/Perfil1.png";
+
     }
     else if(personagem == 1){
         fotoPerfilEl.src = "imgs/Perfil3.png";
+
     }
     else if(personagem == 2){
         fotoPerfilEl.src = "imgs/Perfil6.png";
+
     }
     else if(personagem == 3){
         fotoPerfilEl.src = "imgs/Perfil4.png";
+
     }
     else if(personagem == 4){
         fotoPerfilEl.src = "imgs/Perfil2.png";
+
     }
     else if(personagem == 5){
         fotoPerfilEl.src = "imgs/Perfil5.png";
+
     }
     else if(personagem == 6){
         fotoPerfilEl.src = "imgs/Perfil7.png";
+
     }
 
     usuarios[usuario_atual].foto_perfil = personagem;
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 }
+
+carregar();
+selecionar_foto.addEventListener("change", mudar_foto_perfil(selecionar_foto.value));
