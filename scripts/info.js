d84216a6b73f2c85f8d4890799d114816aeb1022
Filipe Davi <filipedavi80@gmail.com>
2022-02-04 11:49:18 -0300

    Revert "Update index.html"
    This reverts commit e594ac8acc58c5eae01d567b9aa3244e9c61b8ac.

--- scripts/info.js
+++ scripts/info.js
@@ -15,5 +15,3 @@ let p = document.getElementById('p');
 
 h2.innerHTML = 'O projeto';
 h2.style.fontSize = '3rem';
-
-p.innerHTML = 'Basicamente, o projeto consiste em um simples joguinho de perguntas e respostas. Tudo é feito dinamicamente com JavaScript. Começando do começo, o JS principal é o quiz, que seleciona a quantidade de perguntas que o programador julga necessário a ser respondidas. Há, no arquivo banco.json, todas as perguntas e o const MAX define o máximo de perguntas a serem sorteadas desse banco. A cada pergunta acertada, o usuário ganha 10 pontinhos. Logando na aplicação, o jogador poderá, através do webstorage, guardar seu nome de usuário de a quantidade total dos seus pontos, que poderá ser vista tanto no ranking – com usuários genéricos – quanto no seu perfil.>'
